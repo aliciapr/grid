5f2a7d6159d425904eb349997a53df4867747407
ALicia Perez <pericia@gmail.com>
2013-02-05 15:58:32 -0500

    - Update jquery from 1.8.2 to 1.9.0 - Create a table in each div (createGrid and createPalette)

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
 
 	// var containerName = "mainContainer"; createMainContainer(containerName);
 
-	// $('#mainContainer').css('border', '2px solid blue');
+	// $(".main").css('border', '2px solid blue');
 
 	$('#mainContainer').coloredGrid();
+
+	$('#secondMainContainer').coloredGrid();
+	$('#thirdMainContainer').coloredGrid();
 
 });
--- src/plugins.js
+++ src/plugins.js
@@ -4,32 +4,87 @@
 		var mainHeight = this.height();
 		var mainWidth = this.width();
 
+		this.createGrid(mainHeight, mainWidth);
+		this.createPalette(mainHeight, mainWidth);
+		return this;
+	};
+})(jQuery);
+
+(function($) {
+	$.fn.createGrid = function(height, width) {
+
 		var firstDiv = jQuery('<div/>', {
 			id : 'firstDiv'
 		}).appendTo(this);
 		firstDiv.css({
 			'border' : '4px solid salmon',
 			'float' : 'left',
-			'border-radius' : '22%',
-			'width' : (mainWidth * 0.9) - 8, // 4px per border, therefore 8
-			'height' : mainHeight
+			'border-radius' : '10%',
+			'width' : (width * 0.9) - 8, // 4px per border, therefore 8
+			'height' : height - 8
+		// same with height, to enclose the child in the parent
 		});
+
+		var tableName = "myTable_" + this.attr('id');
+		firstDiv.append('<table id="' + tableName + '"'
+				+ ' border="1" "><tbody></tbody><table>');
+		tableName = '#' + tableName;
+		$(tableName).css({
+			'height' : '100%',
+			'width' : '100%'
+		});
+		firstDiv.createTable($(tableName), 20, 20);
+
+		return this;
+	};
+})(jQuery);
+
+(function($) {
+	$.fn.createPalette = function(height, width) {
 
 		var secondDiv = jQuery('<div/>', {
 			id : 'secondDiv'
 		}).appendTo(this);
-		var secondWidth = mainWidth * 0.1;
+		var secondWidth = width * 0.1;
 		var margin = secondWidth * 0.05;
 		var rest = secondWidth - margin;
 		secondDiv.css({
-			'float' : 'left',
+			'float' : 'right',
 			'margin-left' : margin,
-			'border-radius' : '22%',
-			'border' : '4px solid salmon',// lightsalmon
+			'border-radius' : '10%',
+			'border' : '4px solid lightsalmon',
 			'width' : rest - 8,
-			'height' : mainHeight
+			'height' : height - 8
 		});
 
+		// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+		var tableName = "my2ndTable_" + this.attr('id');
+		secondDiv.append('<table id="' + tableName + '"'
+				+ ' border="1" "><tbody></tbody><table>');
+		tableName = '#' + tableName;
+		$(tableName).css({
+			'height' : '100%',
+			'width' : '100%'
+		});
+		secondDiv.createTable($(tableName), 6, 2);
+
+		return this;
+	};
+})(jQuery);
+
+(function($) {
+	$.fn.createTable = function(tbody, rows, cols) {
+		var cellText;
+
+		for ( var r = 0; r < rows; r++) {
+			var trow = $("<tr>");
+			for ( var c = 0; c < cols; c++) {
+				// cellText = "Cell " + r + "." + c;
+				$("<td>").text(cellText).data("col", c).appendTo(trow);
+			}
+			trow.appendTo(tbody);
+		}
+		// $("tr:odd").css('background', '#ccc');
 		return this;
 	};
 })(jQuery);
