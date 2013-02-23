fb4bc0791e180360fe9cdc85caaa90cebb3c62be
ALicia Perez <pericia@gmail.com>
2013-02-23 18:04:25 -0500

    Current version with errors in Palette

--- src/main.js
+++ src/main.js
@@ -3,10 +3,9 @@ $(document).ready(function() {
 	// var containerName = "mainContainer"; createMainContainer(containerName);
 
 	// $(".main").css('border', '2px solid blue');
-
 	$('#mainContainer').coloredGrid();
 
-	$('#secondMainContainer').coloredGrid();
-	$('#thirdMainContainer').coloredGrid();
+	// $('#secondMainContainer').coloredGrid();
+	// $('#thirdMainContainer').coloredGrid();
 
 });
--- src/plugins.js
+++ src/plugins.js
@@ -4,25 +4,33 @@
 		var mainHeight = this.height();
 		var mainWidth = this.width();
 
-		this.createGrid(mainHeight, mainWidth);
+		this.createCanvas(mainHeight, mainWidth);
 		this.createPalette(mainHeight, mainWidth);
+
 		return this;
 	};
 })(jQuery);
 
 (function($) {
-	$.fn.createGrid = function(height, width) {
+	$.fn.createCanvas = function(height, width) {
 
 		var firstDiv = jQuery('<div/>', {
 			id : 'firstDiv'
 		}).appendTo(this);
+		var borderWidth = parseFloat(this.css("border-top-width"))
+				+ parseFloat(this.css("border-bottom-width"));
+		var borderHeight = parseFloat(this.css("border-left-width"))
+				+ parseFloat(this.css("border-right-width"));
+		var fWidth = (width * 0.9) - borderWidth; // (width * 0.9) - 8 //4px
+		// per border, therefore 8
+		var fHeight = height - borderHeight; // height - 8;
 		firstDiv.css({
+			// 'display' : 'table',
 			'border' : '4px solid salmon',
 			'float' : 'left',
 			'border-radius' : '10%',
-			'width' : (width * 0.9) - 8, // 4px per border, therefore 8
-			'height' : height - 8
-		// same with height, to enclose the child in the parent
+			'width' : fWidth,
+			'height' : fHeight
 		});
 
 		var tableName = "myTable_" + this.attr('id');
@@ -30,11 +38,32 @@
 				+ ' border="1" "><tbody></tbody><table>');
 		tableName = '#' + tableName;
 		$(tableName).css({
-			'height' : '100%',
+			// ,'table-layout' : 'fixed'
+			// 'height' : '100%',
+			'border-collapse' : 'collapse',
 			'width' : '100%'
 		});
-		firstDiv.createTable($(tableName), 20, 20);
 
+		firstDiv.createTable($(tableName), 20, 20, fHeight);
+
+		return this;
+	};
+})(jQuery);
+(function($) {
+	$.fn.createTable = function(tbody, rows, cols, totalHeight) {
+		var r, c, trow;
+		var aux = totalHeight / rows;
+		for (r = 0; r < rows; r++) {
+			trow = $("<tr>");
+			for (c = 0; c < cols; c++) {
+				// $("<td>").text("Cell " + r + "," + c).data("col",
+				// c).appendTo(trow);
+				$("<td>").data("col", c).height(aux).appendTo(trow);// .css('height',
+				// aux)
+			}
+			trow.appendTo(tbody);
+		}
+		// $("tr:odd").css('background', '#ccc');
 		return this;
 	};
 })(jQuery);
@@ -46,45 +75,34 @@
 			id : 'secondDiv'
 		}).appendTo(this);
 		var secondWidth = width * 0.1;
-		var margin = secondWidth * 0.05;
-		var rest = secondWidth - margin;
+		var leftMargin = secondWidth * 0.05; // gap of 5% of the containing
+		// div's width
+		var rest = secondWidth - leftMargin;
+		// - parseFloat($('#firstDiv').css("border-right-width"));
+		var borderWidth = parseFloat(this.css("border-top-width"))
+				+ parseFloat(this.css("border-bottom-width"));
+		var borderHeight = parseFloat(this.css("border-left-width"))
+				+ parseFloat(this.css("border-right-width"));
+		var sHeight = height - borderHeight;
 		secondDiv.css({
 			'float' : 'right',
-			'margin-left' : margin,
-			'border-radius' : '10%',
+			'margin-left' : leftMargin,
+			// 'border-radius' : '10%',
 			'border' : '4px solid lightsalmon',
-			'width' : rest - 8,
-			'height' : height - 8
+			'width' : rest - borderWidth,
+			'height' : sHeight
 		});
 
-		// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 		var tableName = "my2ndTable_" + this.attr('id');
 		secondDiv.append('<table id="' + tableName + '"'
 				+ ' border="1" "><tbody></tbody><table>');
 		tableName = '#' + tableName;
 		$(tableName).css({
-			'height' : '100%',
+			'border-collapse' : 'collapse',
 			'width' : '100%'
 		});
-		secondDiv.createTable($(tableName), 6, 2);
+		secondDiv.createTable($(tableName), 6, 2, sHeight);
 
-		return this;
-	};
-})(jQuery);
-
-(function($) {
-	$.fn.createTable = function(tbody, rows, cols) {
-		var cellText;
-
-		for ( var r = 0; r < rows; r++) {
-			var trow = $("<tr>");
-			for ( var c = 0; c < cols; c++) {
-				// cellText = "Cell " + r + "." + c;
-				$("<td>").text(cellText).data("col", c).appendTo(trow);
-			}
-			trow.appendTo(tbody);
-		}
-		// $("tr:odd").css('background', '#ccc');
 		return this;
 	};
 })(jQuery);
